7825ab0d47df2b1fc518032794547165206c57e8
GuoJiang <guojiang@elex-tech.com>
2015-10-21 11:27:59 +0800

    android

--- cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tymacpro/Documents/newbranch2/root/src/client/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
-/Users/tymacpro/Documents/newbranch2/root/src/client/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
