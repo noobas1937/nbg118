5175dac5c2f2a7e136881554036cb111bff8dc9e
GuoJiang <guojiang@elex-tech.com>
2015-10-22 15:07:23 +0800

    世界地图图片

---
+++ IF/Classes/scene/world/map/NBWorldMapMainCity.hpp
@@ -0,0 +1,29 @@
+//
+//  NBWorldMapMainCity.hpp
+//  IF
+//
+//  Created by gj on 10/22/15.
+//
+//
+
+#ifndef NBWorldMapMainCity_hpp
+#define NBWorldMapMainCity_hpp
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class WorldCityInfo;
+
+// worldcastle.xls
+class NBWorldMapMainCity
+{
+public:
+    static int getMainCityIslandImageIndex(const WorldCityInfo* info, int level, int nSpecialId);
+    static const char * getMainCityIslandImage(int island_index, int x, int y);
+    static Node * getMainCity(int island_index, int level, int nSpecialId);
+    
+protected:
+    static int getMainCityId(int level, bool isKing, int nSpecialId);
+};
+
+#endif /* NBWorldMapMainCity_hpp */
