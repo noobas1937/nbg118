d429611f9af38fca99f4f63fbd4b801545093894
tao.yu <441066277@qq.com>
2015-10-26 20:45:27 +0800

    更改地形和建筑ccb

--- IF/Classes/scene/cropscene/ImperialScene.cpp
+++ IF/Classes/scene/cropscene/ImperialScene.cpp
@@ -581,8 +581,6 @@ void ImperialScene::buildingCallBack(CCObject* params)
 
     
     GameController::getInstance()->enableQueryHistoryPurchase();
-    
-    wallCallBack(nullptr);
 }
 
 void ImperialScene::onCreateTitan()
@@ -742,7 +740,7 @@ void ImperialScene::onEnter()
     
     CCLoadSprite::doLoadResourceAsync(IMPERIAL_PATH, CCCallFuncO::create(this, callfuncO_selector(ImperialScene::buildingCallBack), NULL), 2+10*GlobalData::shared()->contryResType);
 
-//    CCLoadSprite::doLoadResourceAsync(IMPERIAL_PATH, CCCallFuncO::create(this, callfuncO_selector(ImperialScene::wallCallBack), NULL), 8+10*GlobalData::shared()->contryResType);
+    CCLoadSprite::doLoadResourceAsync(IMPERIAL_PATH, CCCallFuncO::create(this, callfuncO_selector(ImperialScene::wallCallBack), NULL), 8+10*GlobalData::shared()->contryResType);
     
     CCLoadSprite::doLoadCommonResourceAsync();
     
