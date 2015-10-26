8f1deb3926a274b9d31bec790a30d68e5253b30b
GuoJiang <guojiang@elex-tech.com>
2015-10-26 14:51:52 +0800

    资源

--- proj.android/jni/Sources.mk
+++ proj.android/jni/Sources.mk
@@ -526,7 +526,7 @@ LOCAL_SRC_FILES := main.cpp \
 ../../IF/Classes/scene/world/IFShakeGuideLayer.cpp \
 ../../IF/Classes/scene/world/IFShakeLayer.cpp \
 ../../IF/Classes/scene/world/map/CloudLayer.cpp \
-../../IF/Classes/scene/world/map/NBWaterShaderLayer.cpp \
+../../IF/Classes/scene/world/map/NBWaterSprite.cpp \
 ../../IF/Classes/scene/world/map/NBWorldMapDecoration.cpp \
 ../../IF/Classes/scene/world/map/NBWorldMapMainCity.cpp \
 ../../IF/Classes/scene/world/map/NBWorldMonster.cpp \
