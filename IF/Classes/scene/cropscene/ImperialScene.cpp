1c7b45e0000a6d9cc4e50a9fc2a5d10d38525181
tao.yu <441066277@qq.com>
2015-10-27 14:07:20 +0800

    暂时去掉瀑布

--- IF/Classes/scene/cropscene/ImperialScene.cpp
+++ IF/Classes/scene/cropscene/ImperialScene.cpp
@@ -840,7 +840,7 @@ void ImperialScene::onEnter()
 void ImperialScene::showWaterfall()
 {
 //    auto waterfall = ParticleController::createParticle(CCString::createWithFormat("Drop_%d",i)->getCString());
-    CCBLoadFile("waterfall",this,this);
+//    CCBLoadFile("waterfall",this,this);
 //    m_waterfallNode->addChild(waterfall);
 }
 
