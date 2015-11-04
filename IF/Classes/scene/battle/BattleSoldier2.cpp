a4f53cdc10e12eedb664d5efa2a0e45428ee0f62
GuoJiang <guojiang@elex-tech.com>
2015-11-04 17:37:11 +0800

    主城内列阵士兵调小，加入随机动画

--- IF/Classes/scene/battle/BattleSoldier2.cpp
+++ IF/Classes/scene/battle/BattleSoldier2.cpp
@@ -306,6 +306,17 @@ void BattleSoldier2::delayPlayAnimation(){
         myArray.pushBack(cf);
         m_iconSpr->setDisplayFrame(cf);
     }
+    if (m_status == S_STAND)
+    {
+        int j = 0;
+        int cnt = 10 + rand() % 20;
+        for (int i = 0; i < cnt; i++)
+        {
+            cf = CCLoadSprite::getSF(CCString::createWithFormat("%s_%d_%s_%s_%d.png",m_icon.c_str(),m_side,m_direct.c_str(),actionName.c_str(),j)->getCString());
+            myArray.pushBack(cf);
+            m_iconSpr->setDisplayFrame(cf);
+        }
+    }
     
     m_iconSpr->setColor(ccWHITE);
     float delayPerUnit = 0.1;
