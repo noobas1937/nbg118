86c90168f39845ed0624c8f1f080d604129668be
tao.yu <441066277@qq.com>
2015-11-11 17:57:41 +0800

    重置更改

--- IF/Classes/model/TypeDefinition.cpp
+++ IF/Classes/model/TypeDefinition.cpp
@@ -14,24 +14,24 @@
 //#define CHANGE_SERVER 2
 
 #if CHANGE_SERVER == 1 // 本地服
-    #define __SERVER__IP     "s1.dc.elexapp.com"
-    #define __SERVER__PORT   80
-    #define __SERVER__ZONE   "COK1"
-    #define __SERVERLIST__IP "p1dc.elexapp.com"
-
-    #define __SERVER__IP_2     "s1.dc.elexapp.com"
-    #define __SERVER__PORT_2   80
-    #define __SERVER__ZONE_2   "COK1"
-    #define __SERVERLIST__IP_2 "p1dc.elexapp.com"
-//    #define __SERVER__IP     "10.1.4.106"
+//    #define __SERVER__IP     "s1.dc.elexapp.com"
 //    #define __SERVER__PORT   80
 //    #define __SERVER__ZONE   "COK1"
-//    #define __SERVERLIST__IP "10.1.4.106"
+//    #define __SERVERLIST__IP "p1dc.elexapp.com"
 //
-//    #define __SERVER__IP_2     __SERVER__IP
-//    #define __SERVER__PORT_2   __SERVER__PORT
-//    #define __SERVER__ZONE_2   __SERVER__ZONE
-//    #define __SERVERLIST__IP_2 __SERVERLIST__IP
+//    #define __SERVER__IP_2     "s1.dc.elexapp.com"
+//    #define __SERVER__PORT_2   80
+//    #define __SERVER__ZONE_2   "COK1"
+//    #define __SERVERLIST__IP_2 "p1dc.elexapp.com"
+    #define __SERVER__IP     "10.1.4.106"
+    #define __SERVER__PORT   80
+    #define __SERVER__ZONE   "COK1"
+    #define __SERVERLIST__IP "10.1.4.106"
+
+    #define __SERVER__IP_2     __SERVER__IP
+    #define __SERVER__PORT_2   __SERVER__PORT
+    #define __SERVER__ZONE_2   __SERVER__ZONE
+    #define __SERVERLIST__IP_2 __SERVERLIST__IP
 #elif CHANGE_SERVER == 2 // 测试服
     #define __SERVER__IP     "10.1.4.121"
     #define __SERVER__PORT   80
