efed1149282038131f3004ff4f1ff98b2a2e9e07
GuoJiang <guojiang@elex-tech.com>
2015-11-12 15:23:58 +0800

    动态更新脚本 1. 资源分类 iOS/Android 2. 资源配置的生成 : 每个文件一个配置

--- tools/DLC/genDLC.py
+++ tools/DLC/genDLC.py
@@ -7,34 +7,43 @@ import os, os.path, errno
 import sys
 import shutil
 import plistlib
-
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
 from datetime import datetime
 
+platform_iOS = 'iOS'
+platform_Android = 'Android'
+
 def getCurrentTime():
-   currentSecond= datetime.now().second
-   currentMinute = datetime.now().minute
-   currentHour = datetime.now().hour
+    currentSecond= datetime.now().second
+    currentMinute = datetime.now().minute
+    currentHour = datetime.now().hour
 
-   currentDay = datetime.now().day
-   currentMonth = datetime.now().month
-   currentYear = datetime.now().year
+    currentDay = datetime.now().day
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
 
-   ret = "%4d%02d%02d%02d%02d%02d" % (currentYear, currentMonth, currentDay, currentHour, currentMinute, currentSecond)
-   return ret
+    ret = "%4d%02d%02d%02d%02d%02d" % (currentYear, currentMonth, currentDay, currentHour, currentMinute, currentSecond)
+    return ret
 
-def gen(url, dstFolderName):
+def gen(url, dstFolderName, platform):
     srcPath = os.path.split(os.path.realpath(__file__))[0] + '/../../IF/Resources/'
-    dstPath = os.path.split(os.path.realpath(__file__))[0] + '/../../Resources_tmp/' + dstFolderName + '/'
-    infoPlist = os.path.split(os.path.realpath(__file__))[0] + '/../../IF/Resources/Info.plist'
+    dstPath = os.path.split(os.path.realpath(__file__))[0] + '/../../Resources_download_contents/' + platform + '_' + dstFolderName + '/'
 
-    # zipFiles.zipAllFolders(srcPath, dstPath)
-    if os.path.exists(dstPath):
-        shutil.rmtree(dstPath)
-    # os.makedirs(dstPath)
-    shutil.copytree(srcPath, dstPath)
-
-    pl = plistlib.readPlist(infoPlist)
-    appVer = pl['CFBundleShortVersionString']
+    appVer = '0.0.1'
+    if platform_Android == platform:
+        srcPath = os.path.split(os.path.realpath(__file__))[0] + '/../../proj.android/assets/'
+        xml = os.path.split(os.path.realpath(__file__))[0] + '/../../Publish.android/IF_Global/AndroidManifest.xml'        
+        tree = ET.parse(xml)
+        root = tree.getroot()
+        appVer = root.attrib['{http://schemas.android.com/apk/res/android}versionName']
+    else:
+        infoPlist = os.path.split(os.path.realpath(__file__))[0] + '/../../IF/Resources/Info.plist'
+        pl = plistlib.readPlist(infoPlist)
+        appVer = pl['CFBundleShortVersionString']
+    
     url = url + appVer
     clientMainManifest = 'main.manifest'
     serverMainManifest = 'main_server.manifest'
@@ -47,7 +56,14 @@ def gen(url, dstFolderName):
     genManifests.replaceMap["@ENGINE_VERSION"] = '3.7'
     genManifests.replaceMap["@ASSETS"] = ''
 
+    if os.path.exists(dstPath):
+        shutil.rmtree(dstPath)
+    shutil.copytree(srcPath, dstPath)
     genManifests.exportManifests(dstPath, srcPath + 'manifest/', clientMainManifest, serverMainManifest, versionMainManifest)
 
 if __name__ == "__main__":
-    gen(sys.argv[1], sys.argv[2])
+    # packageUrl = url + appVer
+    url = sys.argv[1]
+    dstFolderName = sys.argv[2]
+    platform = sys.argv[3]
+    gen(url, dstFolderName, platform)
---
+++ tools/DLC/genDLC_AndroidDebug.command
@@ -0,0 +1,2 @@
+cd `dirname $0`
+python genDLC.py "http://cnimg.elex.com/ba/debug/" "debug" "Android"
