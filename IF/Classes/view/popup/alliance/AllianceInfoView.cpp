26d19bc82fbe8b1abc3e351ffc2d768db533e77d
TianXiGe <2937891006@qq.com>
2016-01-04 16:00:44 +0800

    [Alliance] 联盟建筑界面

--- IF/Classes/view/popup/alliance/AllianceInfoView.cpp
+++ IF/Classes/view/popup/alliance/AllianceInfoView.cpp
@@ -1023,9 +1023,10 @@ void AllianceFunCell::clickHandle(CCObject *pSender, CCControlEvent event){
             break;
         case 2:
         {
-            CCCommonUtils::flyHint("", "", _lang("E100008"));
-            return;
-            
+//            CCCommonUtils::flyHint("", "", _lang("E100008"));
+//            return;
+//
+            //fusheng 联盟建筑
             //TODO: guojiang 联盟领地入口
             
             unsigned int index = WorldController::getIndexByPoint(WorldController::getInstance()->selfPoint);
