9df5fbe955d8ad6a1013159b62af361b8f8744ba
tao.yu <441066277@qq.com>
2016-01-29 00:40:00 +0800

    talkingData自定义事件，包括启动游戏，账户信息等(优化版)

--- IF/Classes/scene/loading/LoadingScene.cpp
+++ IF/Classes/scene/loading/LoadingScene.cpp
@@ -764,7 +764,7 @@ void LoadingScene::sendCmdGetServerList(CCObject* p){
     string _platformToken = GlobalData::shared()->platformToken;
     string _Country = GlobalData::shared()->fromCountry;
     string param = "";
-    GA_ACCOUNT_COUNTRY(_Country.c_str());
+//    GA_ACCOUNT_COUNTRY(_Country.c_str());
     
     auto currentTime = time(NULL);
     std::string timeStr = CC_ITOA(double(currentTime));
